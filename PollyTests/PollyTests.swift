0b2e56b803d1744996bb06277c38f85dd871b833
Sean <me@shorn.co.uk>
2026-03-18 16:01:20 +0000

    Initial Commit

---
+++ PollyTests/PollyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PollyTests.swift
+//  PollyTests
+//
+//  Created by Sean on 18/03/2026.
+//
+
+import Testing
+
+struct PollyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
